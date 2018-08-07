128649f029cf415125aa6806b8e7f63c200fa292
Michelly da Silva Lopes Ferraz <michelly.ferraz@saraiva.com.br>
2018-08-07 18:45:34 -0300

    Modificações e finalizações

--- assets/js/mkt-place.js
+++ assets/js/mkt-place.js
@@ -6,6 +6,8 @@ $(document).ready(function (e) {
 
     // Coloca os dados do html dentro de uma variável
     var data = headerMenu.data()
+    const conditionsHasMenu = data.hasOwnProperty('links') && data.links != '' && data.links != ' '
+
     data.background.split('#').length == 2 || data.background.length == 0  ? headerMenu.css('background-color', data.background ? data.background : '#ccc') : headerMenu.css('background', `url('${data.background}') center top / cover no-repeat scroll`);
     
     // Adiciona a classe do tema
@@ -15,18 +17,20 @@ $(document).ready(function (e) {
     function menuLinks(links) {
         let content = '';
         typeof (links) == 'object' ? links.forEach(link => {
-            content += `<div><a style="color:${data.textColorMenu};" href="${link.href}">${link.nome}</a></div>`;
+            content += `<li><a style="color:${data.textColorMenu};" href="${link.href}">${link.nome}</a></li>`;
         }) : content += `${links}`
         return content
     }
     // Função para verificar se existe links para serem adicionados na estrutura
     function hasLinks(data) {
-        return data.hasOwnProperty('links') && data.links != '' && data.links != ' ' ? `<div class="container-fluid menu" style="background-color:${data.backgroundMenu ? data.backgroundMenu : '#ddd'};">
+        return conditionsHasMenu ? `<div class="container-fluid menu" style="background-color:${data.backgroundMenu ? data.backgroundMenu : '#ddd'};">
             <div class="row">
                 <div class="container">
-                    <div class="offset-md-4 offset-lg-3">
-                        <div class="menu__content align-items-stretch d-flex flex-column flex-wrap flex-sm-row">
+                    <div class="offset-md-2 offset-lg-3">
+                        <div class="menu__content">
+                            <ul>
                             ${menuLinks(headerMenu.data('links'))}
+                            </ul>
                         </div>
                     </div>
                 </div>
@@ -42,14 +46,14 @@ $(document).ready(function (e) {
             </div>
         </div>
     </div>
-    <div class="container">
-        <div class="row justify-content-start align-items-center box__empresa">
-            <div class="col-12 col-sm-4 col-lg-3 align-items-center">
+    <div class="container ${conditionsHasMenu ? 'hasMenu' : 'noMenu'}">
+        <div class="row justify-content-start box__empresa">
+            <div class="col-12 col-sm-12 col-lg-3 align-self-center align-items-center">
                 <div class="empresa__img d-flex p-2 shadow-sm mx-auto">
                     <img class="align-self-center img-fluid mx-auto" src="${data.empresaImg}" alt="${data.nomeEmpresa}">
                 </div>
             </div>
-            <div class="col-12 col-sm-8 col-lg-9">
+            <div class="col-12 col-sm-12 col-lg-9 align-self-center">
                 <div class="empresa_text">
                     <h2>${data.nomeEmpresa}</h2>
                     <p>${data.textoEmpresa}

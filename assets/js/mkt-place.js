e52c41d4bd1c2881e4e62059bd47c67e1a8a92f4
Daniella Lapelligrini <daniella.lapelligrin@saraiva.com.br>
2018-08-17 16:58:23 -0300

    js logo empresa

--- assets/js/mkt-place.js
+++ assets/js/mkt-place.js
@@ -4,16 +4,16 @@ $(document).ready(function (e) {
     var headerMenu = $('.topo__mkt-place');
 
 
-    // Coloca os dados do html dentro de uma variável
+    // Coloca os dados do html dentro de uma variÃ¡vel
     var data = headerMenu.data()
     const conditionsHasMenu = data.hasOwnProperty('links') && data.links != '' && data.links != ' '
 
     data.background.split('#').length == 2 || data.background.length == 0  ? headerMenu.css('background-color', data.background ? data.background : '#ccc') : headerMenu.css('background', `url('${data.background}') center top / cover no-repeat scroll`);
-    console.log(data)
+    
     // Adiciona a classe do tema
     headerMenu.addClass(data.tema)
 
-    // Função para adicionar os links no menu
+    // FunÃ§Ã£o para adicionar os links no menu
     function menuLinks(links) {
         let content = '';
         typeof (links) == 'object' ? links.forEach(link => {
@@ -21,9 +21,9 @@ $(document).ready(function (e) {
         }) : content += `${links}`
         return content
     }
-    // Função para verificar se existe links para serem adicionados na estrutura
+    // FunÃ§Ã£o para verificar se existe links para serem adicionados na estrutura
     function hasLinks(data) {
-        return conditionsHasMenu ? `<div class="container-fluid menu" style="background-color:${data.backgroundMenu ? data.backgroundMenu : '#ddd'}">
+        return conditionsHasMenu ? `<div class="container-fluid menu" style="background-color:${data.backgroundMenu ? data.backgroundMenu : '#ddd'};">
             <div class="row">
                 <div class="container">
                     <div class="offset-md-2 offset-lg-3">
@@ -42,7 +42,7 @@ $(document).ready(function (e) {
     <div class="container">
         <div class="row justify-content-end">
             <div class="saraiva_mkt-place">
-                <img src="./assets/img/logo_mktplace-saraiva@1X.png" alt="">
+                <img src="https://images.livrariasaraiva.com.br/estatico/biblioteca/topo_mktplace/img/saraiva-marketplace.png" alt="">
             </div>
         </div>
     </div>
@@ -55,7 +55,7 @@ $(document).ready(function (e) {
             </div>
             <div class="col-12 col-sm-12 col-lg-9 align-self-center">
                 <div class="empresa_text">
-                    <h2>${data.nomeEmpresa}</h2>
+                    <h1>${data.nomeEmpresa}</h1>
                     <p>${data.textoEmpresa}
                     </p>
                 </div>
